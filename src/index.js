a92a752d85ac9df867d4b62b33b684fad5eaa249
Manuel Vila <mvila@3base.com>
2016-01-26 23:04:08 +0900

    Continue implementation

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import EventEmitterMixin from 'event-emitter-mixin';
 import UniversalLog from 'universal-log';
 import fetch from 'isomorphic-fetch';
+import sleep from 'sleep-promise';
 import parseGitHubURL from 'github-url-to-object';
 import { LocalStore, model, Model, primaryKey, field, createdOn, updatedOn } from 'object-layer';
 let pkg = require('../package.json');
@@ -18,17 +19,22 @@ class Package extends Model {
   @primaryKey() id;
   @field(String, { validators: 'filled' }) name;
   @field(String) description;
+  @field(String) npmURL;
+  @field(String) gitHubURL;
+  @field(Date) visibleOn;
   @field(Date, { validators: 'filled' }) createdOn;
   @field(Date, { validators: 'filled' }) updatedOn;
-  @field(Object) rawNPMResult;
-  @field(Object) rawGitHubResult;
-  @createdOn() _itemCreatedOn;
-  @updatedOn() _itemUpdatedOn;
+  @field(Object) npmResult;
+  @field(Object) gitHubResult;
+  @createdOn() itemCreatedOn;
+  @updatedOn() itemUpdatedOn;
 }
 
 class Store extends LocalStore {
   @model(AppState) AppState;
-  @model(Package) Package;
+  @model(Package, {
+    indexes: ['name', 'visibleOn']
+  }) Package;
 }
 
 class Application extends EventEmitterMixin() {
@@ -48,7 +54,8 @@ class Application extends EventEmitterMixin() {
     });
 
     this.npmUpdatedPackagesURL = 'http://registry.npmjs.org/-/_view/browseUpdated?group_level=2';
-    this.npmPackageURL = 'http://registry.npmjs.org/';
+    this.npmWebsitePackageURL = 'https://www.npmjs.com/package/';
+    this.npmAPIPackageURL = 'http://registry.npmjs.org/';
     this.gitHubAPIURL = 'https://api.github.com/';
     this.gitHubUsername = 'mvila';
     this.gitHubPersonalAccessToken = '5169f297fa7d627c4b1b5bf1f0e345dbff117198';
@@ -56,14 +63,16 @@ class Application extends EventEmitterMixin() {
 
   async run() {
     await this.initialize();
-    let startDate = new Date(this.state.npmUpdatedPackagesLastDate.valueOf() + 1);
-    let result = await this.fetchUpdatedPackages(startDate);
-    for (let name of result.packages) {
-      console.log(name);
-    }
-    if (result.lastDate) {
-      this.state.npmUpdatedPackagesLastDate = result.lastDate;
-      await this.state.save();
+    // await this.fetchPackage('webpack-config-json');
+    while (true) {
+      let startDate = new Date(this.state.npmUpdatedPackagesLastDate.valueOf() + 1);
+      let result = await this.findUpdatedPackages(startDate);
+      await this.updatePackages(result.packages);
+      if (result.lastDate) {
+        this.state.npmUpdatedPackagesLastDate = result.lastDate;
+        await this.state.save();
+      }
+      await sleep(60 * 1000);
     }
   }
 
@@ -74,69 +83,112 @@ class Application extends EventEmitterMixin() {
     }
   }
 
-  async fetchUpdatedPackages(startDate) {
-    let startKey = [startDate];
-    let url = this.npmUpdatedPackagesURL;
-    url += '&startkey=' + encodeURIComponent(JSON.stringify(startKey));
-    let response = await fetch(url);
-    if (response.status !== 200) {
-      throw new Error(`Bad response from npm registry while fetching updated packages (HTTP status: ${response.status})`);
-    }
-    let result = await response.json();
-    let rows = result.rows;
+  async findUpdatedPackages(startDate) {
     let packages = [];
     let lastDate;
-    for (let row of rows) {
-      packages.push(row.key[1]);
-      lastDate = new Date(row.key[0]);
+    try {
+      let startKey = [startDate];
+      let url = this.npmUpdatedPackagesURL;
+      url += '&startkey=' + encodeURIComponent(JSON.stringify(startKey));
+      let response = await fetch(url);
+      if (response.status !== 200) {
+        throw new Error(`Bad response from npm registry while fetching updated packages (HTTP status: ${response.status})`);
+      }
+      let result = await response.json();
+      let rows = result.rows;
+      for (let row of rows) {
+        packages.push(row.key[1]);
+        lastDate = new Date(row.key[0]);
+      }
+    } catch (err) {
+      this.log.warning(`An error occured while fetching updated packages from npm registry (${err.message})`);
     }
     return { packages, lastDate };
   }
 
+  async updatePackages(packages) {
+    for (let name of packages) {
+      let pkg = await this.fetchPackage(name);
+      if (!pkg) continue;
+      let packages = this.store.Package.find({ query: { name }, limit: 1 });
+      let isNew;
+      let item = packages[0];
+      if (!item) {
+        item = new this.store.Package();
+        isNew = true;
+      }
+      Object.assign(item, pkg);
+      if (pkg.visible) {
+        if (!item.visibleOn) item.visibleOn = new Date();
+      } else {
+        if (item.visibleOn) { // eslint-disable-line no-lonely-if
+          this.log.info(`'${name}' package became invisible`);
+          item.visibleOn = undefined;
+        }
+      }
+      await item.save();
+      this.log.info(`'${name}' package ${isNew ? 'created' : 'updated'} (${pkg.visible ? 'visible' : 'invisible'})`);
+    }
+  }
+
   async fetchPackage(name) {
     try {
-      let url = this.npmPackageURL + name;
+      let npmURL = this.npmWebsitePackageURL + name;
+      let url = this.npmAPIPackageURL + name;
       let response = await fetch(url);
       if (response.status !== 200) {
         this.log.warning(`Bad response from npm registry while fetching '${name}' package (HTTP status: ${response.status})`);
         return undefined;
       }
-      let result = await response.json();
+      let npmResult = await response.json();
       let gitHubResult;
       let parsedGitHubURL;
-      if (result.repository && result.repository.type === 'git') {
-        parsedGitHubURL = parseGitHubURL(result.repository.url);
-      }
-      if (!parsedGitHubURL) {
-        this.log.notice(`'${name}' package has invalid or empty GitHub URL`);
-      }
-      let promote = this.getPromoteProperty(result);
-      if (promote === false) return undefined;
-      if (!promote) {
-        if (!parsedGitHubURL) return undefined;
-        gitHubResult = await this.fetchGitHubRepository(parsedGitHubURL);
-        let gitHubStars = gitHubResult && gitHubResult.stargazers_count;
-        if (gitHubStars == null) return undefined;
-        if (gitHubStars < 3) {
-          this.log.info(`'${name}' package has not enough stars (${gitHubStars}<3)`);
-          return undefined;
+      if (npmResult.repository && npmResult.repository.type === 'git') {
+        if (npmResult.repository.url.includes('github')) {
+          parsedGitHubURL = parseGitHubURL(npmResult.repository.url);
+          if (parsedGitHubURL) {
+            gitHubResult = await this.fetchGitHubRepository(parsedGitHubURL);
+          } else {
+            this.log.notice(`'${name}' package has an invalid GitHub URL (${npmResult.repository.url})`);
+          }
+        } else {
+          this.log.notice(`'${name}' package has a Git respository not hosted by GitHub (${npmResult.repository.url})`);
         }
+      } else {
+        this.log.notice(`'${name}' package doesn't have a Git respository`);
       }
-      let createdOn = result.time && result.time.created && new Date(result.time.created);
-      let updatedOn = result.time && result.time.modified && new Date(result.time.modified);
+      let visible = this.checkVisibility(name, npmResult, gitHubResult);
       let pkg = {
-        name: result.name,
-        description: result.description,
-        createdOn,
-        updatedOn,
-        rawNPMResult: result,
-        rawGitHubResult: gitHubResult
+        name: npmResult.name,
+        description: npmResult.description,
+        npmURL,
+        gitHubURL: parsedGitHubURL && parsedGitHubURL.https_url,
+        visible,
+        createdOn: npmResult.time && npmResult.time.created && new Date(npmResult.time.created),
+        updatedOn: npmResult.time && npmResult.time.modified && new Date(npmResult.time.modified),
+        npmResult,
+        gitHubResult
       };
       return pkg;
     } catch (err) {
       this.log.warning(`An error occured while fetching '${name}' package from npm registry (${err.message})`);
       return undefined;
     }
+  }
+
+  checkVisibility(name, npmResult, gitHubResult) {
+    let promote = this.getPromoteProperty(npmResult);
+    if (promote === false) return false;
+    if (!promote) {
+      if (!gitHubResult) return false;
+      let gitHubStars = gitHubResult.stargazers_count;
+      if (gitHubStars == null) return false;
+      if (gitHubStars < 3) {
+        this.log.info(`'${name}' package has not enough stars (${gitHubStars}<3)`);
+        return false;
+      }
+    }
+    return true;
   }
 
   getPromoteProperty(data) {
