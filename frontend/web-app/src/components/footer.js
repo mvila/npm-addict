68f9442ba98b6023bd4ce99f1cd2f30a60dcc36c
Manuel Vila <mvila@3base.com>
2016-02-09 07:44:44 +0900

    Add GitHub link

--- frontend/web-app/src/components/footer.js
+++ frontend/web-app/src/components/footer.js
@@ -7,10 +7,15 @@ import s from '../styles';
 @Radium
 export class Footer extends React.Component {
   render() {
+    let separator = <span style={[s.ml1, s.mr1, s.grayLight]}>|</span>;
     return (
       <footer style={[s.mb2, s.center, s.fs5, s.grayDarker, this.props.style]}>
         <div>
-          <a href={'#/feeds'}>RSS Feeds</a><span style={[s.ml1, s.mr1, s.grayLight]}>|</span><a href='https://twitter.com/npmaddict'>Twitter</a>
+          <a href={'#/feeds'}>RSS Feeds</a>
+          {separator}
+          <a href='https://twitter.com/npmaddict'>Twitter</a>
+          {separator}
+          <a href='https://github.com/mvila/npm-addict'>GitHub</a>
         </div>
         <div style={[s.mt05]}>
           <span style={[s.fs6]}>Crafted in Japan by <a href='https://github.com/mvila'>Manuel Vila</a></span>
