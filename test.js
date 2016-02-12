62d646d9cbfd93c690a10367c7c0b35e29c0fefe
Manuel Vila <mvila@3base.com>
2016-02-12 16:41:57 +0900

    Remove leaked Twitter credentials :(

--- test.js
+++ test.js
@@ -7,18 +7,3 @@
 //
 //   });
 // });
-
-let Twit = require('twit');
-
-let twit = new Twit({
-  consumer_key: 'stQM0CNOLnbAuOg2G0t0HhvQF', // eslint-disable-line
-  consumer_secret: 'URbIolavIxXutcmJyHCzREoKJbV92vcFFY7uartWhbFAZLaf3z', // eslint-disable-line
-  access_token: '4841972786-M9ThYtA11VmrADpJAzidOwnwfG0mJs3uM5JBexR', // eslint-disable-line
-  access_token_secret: 'k98dE8eV1u5qyzapiZpvi0s1AyN8rTSPZkXjpsR99oiOg', // eslint-disable-line
-  timeout_ms: 60*1000 // eslint-disable-line
-});
-
-twit.post('statuses/update', { status: 'Lorem ipsum' }, function(err, data) {
-  console.log(err);
-  console.log(data);
-});
