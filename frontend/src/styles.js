5aaa19f6d5ec567d43aebb989bec444389532b65
Manuel Vila <mvila@3base.com>
2016-02-19 09:42:56 +0900

    Improve styles.js

--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -2,9 +2,11 @@
 
 import RadiumStarter from 'radium-starter';
 
-export let styles = new RadiumStarter({
+let starter = new RadiumStarter({
   $primaryColor: '#CB3837', // red
   $secondaryColor: '#0074D9' // blue
 });
+
+export let styles = starter;
 
 export default styles;
