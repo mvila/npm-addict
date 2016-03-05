f0b255a6aa6ab40ecb5f6f3b9b7b69f1e394ad1a
Manuel Vila <mvila@3base.com>
2016-03-06 08:28:52 +0900

    Hide the GitHub icon when there is no GitHub link

--- frontend/src/components/package-item.js
+++ frontend/src/components/package-item.js
@@ -28,7 +28,11 @@ export class PackageItem extends React.Component {
             this.Radium.getState(this.state, 'line', ':hover') ?
             <span>
               <a href={item.npmURL} target='_blank'><img key='npmLink' src='images/npm-logo-black.png' alt='npm' width='15' height='15' style={[{ marginLeft: 12 }, MINI_ICON_STYLE]} /></a>
-              <a href={item.gitHubURL} target='_blank'><img key='gitHubLink' src='images/github-logo-black.png' alt='GitHub' width='16' height='16' style={[{ marginLeft: 8 }, MINI_ICON_STYLE]} /></a>
+              {
+                item.gitHubURL ?
+                <a href={item.gitHubURL} target='_blank'><img key='gitHubLink' src='images/github-logo-black.png' alt='GitHub' width='16' height='16' style={[{ marginLeft: 8 }, MINI_ICON_STYLE]} /></a> :
+                null
+              }
             </span> :
             null
           }
